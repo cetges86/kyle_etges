b0681886bba1f4cc5db7c1c05559cddcfd0b75b7
Christopher Etges <c.etges86@gmail.com>
2018-08-12 19:38:03 -0600

    add calendar

--- kyle-etges/client/src/pages/MainPage/MainPage.js
+++ kyle-etges/client/src/pages/MainPage/MainPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import "./main.css";
+import Calendar from "react-calendar";
+
 
 const MainPage = () => {
     return (
         <div className="grid-wrapper">
           <div className="center">Events Calendar</div>
           <div className="center">Latest News</div>
+          <div className="calendar">
+              <Calendar />
+              </div>
         </div>
     )
 }
-
 
 
 export default MainPage;
