059101aad5e4ce2f7b619f122ccebcc393f3fc11
Christopher Etges <c.etges86@gmail.com>
2018-08-19 21:08:24 -0600

    bio page paragraphs

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
         <footer>
             <div className="footer">
                 <h4 className="footer-follow">Follow</h4>
-                <i className="icon icon-1 fab fa-facebook-f"></i>
-                <i className="icon icon-2 fab fa-instagram"></i>
-                <i className="icon icon-3 fab fa-linkedin-in"></i>
+                <i className="icon fb-icon fab fa-facebook-f"></i>
+                <i className="icon ig-icon fab fa-instagram"></i>
+                <i className="icon li-icon fab fa-linkedin-in"></i>
             </div>
-            <div className="white center">&copy; 2018 Chris Etges</div>
+            <div className="white small-text center">&copy; 2018 Chris Etges Design &amp; Development</div>
         </footer>
     )
 }
