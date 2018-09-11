a16fe8efa8d859b2530a2659f0f9ad4ff39d2d52
Christopher Etges <c.etges86@gmail.com>
2018-09-11 16:40:17 -0600

    links setup

--- client/src/pages/CommissionsPage/Commissions.js
+++ client/src/pages/CommissionsPage/Commissions.js
@@ -5,38 +5,46 @@ import sheetMusic1 from "./sheet_music1.jpg";
 import sheetMusic2 from "./sheet_music2.jpg";
 import saxophone from "./saxophone.jpg";
 import coffee from "./coffee1.jpg";
+import composing from "./composing.jpg"
 
 const Commissions = () => {
     return (
         <div className="page-wrapper">
             <Navbar />
             <div className="commission-banner">
-                <h1 className="center">Commissions</h1>
+                <h1 className="center">Commission Work</h1>
+                <h3 className="center">To commission a transcript from Kyle, click the links below to email Kyle </h3>
             </div>
             <div className="container grid-wrapper">
                 <div className="commission-container">
                     <img className="commission-img" alt="commission-element" src={sheetMusic1} />
                     <div className="hover-box">
-                        <div className="text">Transcription (Full Band) <br />
+                        <div className="text"><a href="mailto:kyle.etges@gmail.com">Transcription (Full Band)</a> <br />
                         $300</div>
                     </div>
                 </div>
                 <div className="commission-container">
                     <img className="commission-img" alt="commission-element" src={sheetMusic2} />
                     <div className="hover-box">
-                        <div className="text">Transcriptions (Full Band)</div>
+                        <div className="text">Transcriptions (Partial) <br /> Starting at $150</div>
                     </div>
                 </div>
                 <div className="commission-container">
                     <img className="commission-img" alt="commission-element" src={saxophone} />
                     <div className="hover-box">
-                        <div className="text">Transcriptions (Full Band)</div>
+                        <div className="text">Transcriptions (Solo)<br />$75</div>
                     </div>
                 </div>
                 <div className="commission-container">
                     <img className="commission-img" alt="commission-element" src={coffee} />
                     <div className="hover-box">
-                        <div className="text">Transcriptions (Full Band)</div>
+                        <div className="text">Compositions (Full Band)<br />$500</div>
+                    </div>
+                </div>
+                <div className="commission-container">
+                    <img className="commission-img" alt="commission-element" src={composing} />
+                    <div className="hover-box">
+                        <div className="text">Compositions (Partial Band)<br />$150</div>
                     </div>
                 </div>
             </div>
