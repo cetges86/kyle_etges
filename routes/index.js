60e8c7dfa53e331afa7d3913ba2eee25c2c4a788
Christopher Etges <c.etges86@gmail.com>
2018-09-28 21:34:16 -0600

    attempt to fix build

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.use(function(req, res) {
   console.log('hit')
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
