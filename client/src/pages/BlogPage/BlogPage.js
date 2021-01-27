70f81e8a93cd3bc83337b813baddad9bce8b035b
Christopher Etges <c.etges86@gmail.com>
2021-01-26 20:43:56 -0700

    add blog post

--- client/src/pages/BlogPage/BlogPage.js
+++ client/src/pages/BlogPage/BlogPage.js
@@ -9,6 +9,7 @@ import avantGarde from './Essential Artists of Avant Garde & Free Jazz.pdf'
 import fjHaydn from './A Schenkerian Analysis of Symphony No. 100,  Movement IV, mm.1-8, by Franz Joseph Haydn.pdf'
 import felaKuti from './The Music and Activism of Fela Kuti, A Selective Annotated Bibliography.pdf'
 import stravinsky from "./Stravinsky's Influence on the Jazz Idiom.pdf"
+import chordScales from "./A Crash Course in Chord_Scales.pdf"
 
 
 const BlogPage = () => {
@@ -19,6 +20,11 @@ const BlogPage = () => {
                 <h1 className="center">Blog</h1>
             </div>
             <div className="grid-wrapper">
+            <div className="blog-post">
+                    <h2>A Crash Course in Chord Scales</h2>
+                    <p>Click the icon to download / view this post</p>
+                    <a id="blog-link" target="_blank" href={chordScales}><i className="fas fa-file-invoice"></i></a>
+                </div>
             <div className="blog-post">
                     <h2>The Rite of Swing_ Stravinsky's Influence on the Jazz Idiom</h2>
                     <p>Click the icon to download / view this post</p>
